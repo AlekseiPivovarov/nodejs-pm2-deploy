199862dadee28847046f6607ee5a05dcfeb68ee2
Aleksei Pivovarov <aleksei-pivovarov_95@mail.ru>
2026-04-05 16:56:17 +0300

    test

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,13 +4,14 @@ const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_SSH_KEY, REPO_URL
 } = process.env;
 
+const localEnvPath = process.cwd().replace(/\\/g, '/') + '/.env';
 
 module.exports = {
   apps: [{
     name: 'api-service',
     script: './dist/app.js',
     cwd: '/home/user/nodejs-pm2-deploy/current/backend',
-    interpreter: "/home/user/.nvm/versions/node/v22.22.2/bin/npm",
+    interpreter: "/home/user/.nvm/versions/node/v22.22.2/bin/node",
   }],
 
   deploy: {
@@ -22,7 +23,7 @@ module.exports = {
       repo: REPO_URL,
       path: DEPLOY_PATH,
       'ssh_options': 'StrictHostKeyChecking=no',
-      'pre-deploy-local': `scp "${process.cwd()}/.env" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend/`,
+      'pre-deploy-local': `scp "${localEnvPath}" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend/`,
       'post-deploy': 'cd backend && export PATH=$PATH:/home/user/.nvm/versions/node/v22.22.2/bin && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
     },
   },
